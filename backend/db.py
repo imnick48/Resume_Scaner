aee6a4096d4fd03bde8fc24fb4b1b5cd102cb537
Sagnick Das <45506982+imnick48@users.noreply.github.com>
2025-03-16 23:32:46 +0530

    Bug fixes

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-DATABASE_URL = "mysql+pymysql://root:Samirdas123@localhost/resume_db"
+DATABASE_URL = ""
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
